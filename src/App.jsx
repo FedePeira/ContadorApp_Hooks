8e3f3bc8d3e8f0f70081c7ea901b4718aa091e1a
Federico Peirano <fedepr2345@gmail.com>
2024-03-22 11:45:58 -0300

    Atributos de Propagacion - Hooks Personalizados - Part7.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,39 @@
 import { useState } from 'react'
 
-const useCounter = () => {
-  const [value, setValue] = useState(0)
+const useField = (type) => {
+  const [value, setValue] = useState('')
 
-  const increase = () => {
-    setValue(value + 1)
-  }
-
-  const decrease = () => {
-    setValue(value - 1)
-  }
-
-  const zero = () => {
-    setValue(0)
+  const onChange = (event) => {
+    setValue(event.target.value)
   }
 
   return {
-    value, 
-    increase,
-    decrease,
-    zero
+    type,
+    value,
+    onChange
   }
 }
 
 const App = () => {
-  const counter = useCounter()
+  const name = useField('text')
+  const born = useField('date')
+  const height = useField('number')
 
   return (
     <div>
-      <div>{counter.value}</div>
-      <button onClick={counter.increase}>
-        plus
-      </button>
-      <button onClick={counter.decrease}>
-        minus
-      </button>      
-      <button onClick={counter.zero}>
-        zero
-      </button>
+      <form>
+        name: 
+        <input  {...name} /> 
+        <br/> 
+        birthdate:
+        <input {...born} />
+        <br /> 
+        height:
+        <input {...height} />
+      </form>
+      <div>
+        {name.value} {born.value} {height.value}
+      </div>
     </div>
   )
 }
